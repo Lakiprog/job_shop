3c6e77217ee515cbf697f15efa406278a5645a3e
Laslo Sabadi Baranji <szabadib.laci999@gmail.com>
2023-09-26 14:37:49 +0200

    PSO update

--- main.py
+++ main.py
@@ -4,14 +4,8 @@ import numpy as np
 
 FILE_PATH = "./data.txt"
 
-machines = np.array([])
-jobs = np.array([], object)
-particles = np.array([], object)
-global_best_position = np.array([])
-global_best_make_span = math.inf
-dimension = 0
-
 # parameters
+population_size = 50
 mutation_probability = 0.01
 C1 = 2.0
 C2 = 2.0
@@ -19,6 +13,14 @@ max_iter = 1000
 inertia_weight = 1.4
 inertia_weight_max = 1.4
 inertia_weight_min = 0.4
+
+# global variables
+machines = np.array([])
+jobs = np.array([], dtype=object)
+particles = np.array([None] * population_size, dtype=object)
+global_best_position = np.array([])
+global_best_make_span = math.inf
+dimension = 0
 
 
 class Operation:
@@ -50,7 +52,7 @@ class PSOParticle:
 
     def __init__(self):
         self.encoded_position = None
-        self.position = np.zeros(dimension)
+        self.position = np.random.uniform(-dimension, dimension, dimension)
         self.velocity = np.zeros(dimension)
         self.make_span = math.inf
         self.local_best_position = np.zeros(dimension)
@@ -60,26 +62,22 @@ class PSOParticle:
         return (f"POSITION={self.position} MAKE SPAN={self.make_span} " +
                 f"LOCAL BEST POSITION={self.local_best_position} LOCAL BEST MAKE SPAN={self.local_best_make_span}\n")
 
-    # updates velocity according to current position and velocity
     def update_velocity(self):
         first_part = self.velocity * inertia_weight
         second_part = C1 * random.random() * (self.local_best_position - self.position)
         third_part = C2 * random.random() * (global_best_position - self.position)
         self.velocity = first_part + second_part + third_part
 
-    # moves particle according to current position and velocity
     def update_position(self):
         self.position += self.velocity
         self.rk_encoding()
         self.make_span = get_make_span_by_position(self.encoded_position)
 
     def rk_encoding(self):
-        # turn integer series into permutation of job indexes
         integer_series = self.position_integer_series()
         for index, value in enumerate(integer_series.copy()):
             integer_series[index] = value % len(jobs)
 
-        # turn job index series into operation sequence
         operation_sequence = np.array([None] * dimension, dtype=Operation)
         job_occurrences = {}
         for index, job_index in enumerate(integer_series):
@@ -87,12 +85,11 @@ class PSOParticle:
                 job_occurrences[job_index] += 1
             else:
                 job_occurrences[job_index] = 0
-            operation_sequence[index] = jobs[job_index].operations[job_occurrences[job_index]]
+            operation_sequence[index] = jobs[int(job_index)].operations[int(job_occurrences[job_index])]
 
         self.encoded_position = operation_sequence
         return operation_sequence
 
-    # turn real numbers into integer series
     def position_integer_series(self):
         integer_series = np.zeros(dimension)
         position_copy = self.position.copy()
@@ -121,44 +118,6 @@ class PSOParticle:
         self.position = np.insert(deleted, indexes[1], temp)
         self.rk_encoding()
 
-    # swapping operation for local search
-    # def swapping_operation(self):
-    #     first_index, second_index, first_operation, second_operation = self.get_randoms_for_local_search()
-    #     operations_copy = self.encoded_position.copy()
-    #     operations_copy[first_index] = second_operation
-    #     operations_copy[second_index] = first_operation
-    #     return operations_copy
-
-    # insertion operation for local search
-    # def insertion_operation(self):
-    #     first_index, second_index, first_operation, second_operation = self.get_randoms_for_local_search()
-    #     operations_copy = self.encoded_position.copy()
-    #     operations_copy.pop(first_index)
-    #     operations_copy.insert(second_index, first_operation)
-    #     return operations_copy
-
-    # Helper function for local searches, gets dimensions and their operations
-    # def get_randoms_for_local_search(self):
-    #     chosen_jobs = random.sample(jobs, 2)
-    #     first_operation = random.sample(chosen_jobs[0].operations, 1)
-    #     second_operation = random.sample(chosen_jobs[1].operations, 1)
-    #
-    #     first_index = 0
-    #     second_index = 0
-    #     first_occurrences_found = 0
-    #     second_occurrences_found = 0
-    #     for index, operation in enumerate(self.encoded_position):
-    #         if operation.job_id == first_operation.job_id:
-    #             if first_occurrences_found == first_operation.operation_id:
-    #                 first_index = index
-    #             first_occurrences_found += 1
-    #         elif operation.job_id == second_operation.job_id:
-    #             if second_occurrences_found == second_operation.operation_id:
-    #                 second_index = index
-    #             second_occurrences_found += 1
-    #     return first_index, second_index, first_operation, second_operation
-
-    # mutate
     def mutation(self):
         if random.random() <= mutation_probability:
             old_position = self.position
@@ -192,7 +151,7 @@ def get_make_span_by_position(position):
             machine["elapsed_time"] = operation.duration
         else:
             added = False
-            for machine_dict in machine_dictionary:
+            for machine_dict in machine_dictionary.values():
                 if machine_dict["current_operation"].job_id == operation.job_id:
                     machine["elapsed_time"] = machine_dict["elapsed_time"] + operation.duration
                     added = True
@@ -201,7 +160,7 @@ def get_make_span_by_position(position):
         machine["current_operation"] = operation
 
     longest_machine_time = 0
-    for machine_dict in machine_dictionary:
+    for machine_dict in machine_dictionary.values():
         if longest_machine_time < machine_dict["elapsed_time"]:
             longest_machine_time = machine_dict["elapsed_time"]
     return longest_machine_time
@@ -225,6 +184,7 @@ def iteration(current_iteration):
     print()
     print(f"ITERATION #{current_iteration}")
     print("############################################")
+
     for particle in particles:
         particle.mutation()
         particle.update_local_best()
@@ -238,11 +198,15 @@ def iteration(current_iteration):
         particle.update_position()
         print(particle.__str__())
 
+    print("############################################")
+
 
 def read_file_dataset(dataset_name):
     global machines
     global jobs
     global dimension
+    global particles
+    global global_best_position
     with open(FILE_PATH, 'r') as f:
         while True:
             line = f.readline()
@@ -269,18 +233,12 @@ def read_file_dataset(dataset_name):
                         dimension += 1
 
                     jobs[job_index] = Job(job_index, operations)
+
+                particles = np.array([PSOParticle()] * population_size)
+                global_best_position = np.zeros(dimension)
+                break
             elif line.find("+ EOF +") != -1:
                 break
-
-
-# def generate_random_problem(self, num_machines, num_jobs):
-#     self.machines = np.arange(num_machines)
-#     times = np.arange(1, 10)
-#     for job_id in np.arange(num_jobs):
-#         operations = []
-#         for machine_id in np.random.permutation(self.machines):
-#             operations.append(Operation(machine_id, job_id, np.random.choice(times)))
-#         jobs.append(Job(job_id, operations))
 
 
 if __name__ == '__main__':
@@ -295,3 +253,9 @@ if __name__ == '__main__':
     for job in jobs:
         print(job.__str__())
     print("############################################")
+
+    for it in range(max_iter):
+        iteration(it)
+
+    print(f"Best position: {global_best_position}")
+    print(f"Best make span: {global_best_make_span}")
